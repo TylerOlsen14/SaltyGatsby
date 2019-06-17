add52251fc483f61d2e584adddaafb4a2fd7e417
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-06-16 22:27:49 -0600

    Flexbox 90% done

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "./header"
-
+import logo from "../images/logo.jpg"
 
 export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ margin: `1rem auto`, maxWidth: 650, padding: `0.1rem` }}>
+  <div className="header" style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-around', alignItems: 'flex-end', backgroundColor: "green"}}>
+    <img src={logo} style={{ width: 50, height: 50 }} />
     <Header />
+  </div>
+
     {children}
   </div>
 )
